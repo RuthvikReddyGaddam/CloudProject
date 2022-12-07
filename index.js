149bc27f3af57adb462a4ecd6948967375b0af1e
Ruthvik Reddy <107829319+RuthvikReddyGaddam@users.noreply.github.com>
2022-12-07 20:39:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
+//         secure: true,
         expires: Date.now() + 1000 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 24 * 7,
         
